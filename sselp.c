c3c53904786edb0c646dafaf15e2382eaf8999d2
Connor Lane Smith <cls@lubutu.com>
2010-08-09 11:58:17 +0100

    simpler + faster

--- sselp.c
+++ sselp.c
@@ -1,67 +1,44 @@
 /* See LICENSE file for license details. */
-#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <X11/Xlib.h>
 #include <X11/Xatom.h>
-#include <X11/Xutil.h>
-
-static unsigned char *
-getsel(unsigned long offset, unsigned long *len, unsigned long *remain) {
-	Display *dpy;
-	Atom utf8_string;
-	Atom xa_clip_string;
-	Window w;
-	XEvent ev;
-	Atom typeret;
-	int format;
-	unsigned char *data;
-	unsigned char *result = NULL;
-
-	dpy = XOpenDisplay(NULL);
-	if(!dpy)
-		return NULL;
-	utf8_string = XInternAtom(dpy, "UTF8_STRING", False);
-	xa_clip_string = XInternAtom(dpy, "_SSELP_STRING", False);
-	w = XCreateSimpleWindow(dpy, DefaultRootWindow(dpy), 10, 10, 200, 200,
-			1, CopyFromParent, CopyFromParent);
-	XConvertSelection(dpy, XA_PRIMARY, utf8_string, xa_clip_string,
-			w, CurrentTime);
-	XFlush(dpy);
-	XNextEvent(dpy, &ev);
-	if(ev.type == SelectionNotify && ev.xselection.property != None) {
-		XGetWindowProperty(dpy, w, ev.xselection.property, offset, 4096L, False,
-				AnyPropertyType, &typeret, &format, len, remain, &data);
-		if(*len) {
-			result = malloc(sizeof(unsigned char) * *len);
-			memcpy(result, data, *len);
-		}
-		XDeleteProperty(dpy, w, ev.xselection.property);
-	}
-	XDestroyWindow(dpy, w);
-	XCloseDisplay(dpy);
-	return result;
-}
 
 int
-main(int argc, char **argv) {
+main(int argc, char *argv[]) {
+	Atom utf8, type;
+	Display *dpy;
+	Window win;
+	XEvent ev;
+	int fmt;
+	long off = 0;
 	unsigned char *data;
-	unsigned long i, offset, len, remain;
+	unsigned long len, more;
 
-	if((argc > 1) && !strncmp(argv[1], "-v", 3)) {
-		fputs("sselp-"VERSION", © 2006-2008 Anselm R Garbe\n", stdout);
-		exit(EXIT_SUCCESS);
+	if(argc > 1 && !strcmp(argv[1], "-v")) {
+		fputs("sselp-"VERSION", © 2006-2010 Anselm R Garbe\n", stdout);
+		return 0;
 	}
-	len = offset = remain = 0;
-	do {
-		data = getsel(offset, &len, &remain);
-		for(i = 0; i < len; i++)
-			putchar(data[i]);
-		offset += len;
-		free(data);
-	}
-	while(remain);
-	if(offset)
+	if(!(dpy = XOpenDisplay(NULL)))
+		return 1;
+
+	utf8 = XInternAtom(dpy, "UTF8_STRING", False);
+	win = XCreateSimpleWindow(dpy, DefaultRootWindow(dpy), 0, 0, 1, 1, 0,
+	                          CopyFromParent, CopyFromParent);
+	XConvertSelection(dpy, XA_PRIMARY, utf8, None, win, CurrentTime);
+
+	XNextEvent(dpy, &ev);
+	if(ev.type == SelectionNotify && ev.xselection.property != None) {
+		do {
+			XGetWindowProperty(dpy, win, utf8, off, BUFSIZ, False,
+			                   utf8, &type, &fmt, &len, &more, &data);
+			fwrite(data, 1, len, stdout);
+			XFree(data);
+			off += len;
+		}
+		while(more > 0);
 		putchar('\n');
+	}
+	XCloseDisplay(dpy);
 	return 0;
 }
